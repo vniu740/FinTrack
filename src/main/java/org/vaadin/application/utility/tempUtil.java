b65387c365ef3739c088142b031b2dc72257fb85
VENXIA <vniu740@aucklanduni.ac.nz>
2024-09-28 01:00:53 +1200

    Revert "refactor: Renamed /examples to /application"
    This reverts commit dc864394f09a09a66ebc14ecf7ad882aa47c4e69.

--- src/main/java/org/vaadin/application/utility/tempUtil.java
+++
@@ -1,7 +0,0 @@
-package org.vaadin.application.utility;
-
-/* The util package contains utility classes and 
-methods that provide common functionality used across the application. */
-public class tempUtil {
-    
-}
